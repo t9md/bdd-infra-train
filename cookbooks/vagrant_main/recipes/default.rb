a7c7c7b02ab4d71b2c0a6230e336c9da57f0497f
t9md <taqumd@gmail.com>
2011-07-31 18:29:44 +0900

    add code to handle default gateway

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -36,6 +36,12 @@ file "/tmp/testfile" do
   hoge"
 end
 
+execute "set_default_route" do
+  command "ip route delete default ; ip route add default via #{node[:my_gateway]}"
+  not_if "ip route  | grep -v grep | grep ^default | grep #{node[:my_gateway]}"
+  action :run
+end
+
 unless node[:hostname] == node[:my_hostname]
   execute "change hostname" do
     command "hostname #{node[:my_hostname]}"
@@ -43,75 +49,15 @@ unless node[:hostname] == node[:my_hostname]
   end
 end
 
-# add repository source to local
-# template "/etc/apt/sources.list" do
-  # owner "root"
-  # group "root"
-  # mode "0644"
-  # source "sources.list.erb"
-# end
-
-# openstack
-# apt_repository "openstack" do
-  # uri "http://172.27.98.16/cloud/latest/openstack/nova-core/release/ubuntu"
-  # distribution node['lsb']['codename']
-  # components ["main"]
-  # # keyserver "keyserver.ubuntu.com"
-  # # key "C300EE8C"
-  # action :add
-# end
-
-
-cron "noop" do
-  hour "5"; minute "0"
-  command "/bin/true"
+service "hostname" do
+  action [:enable, :start]
 end
 
-MY_NAME="TAKU Maeda This is my new name #{Time.now}"
-execute "HOGHEOGE" do
-  command "echo #{MY_NAME} >>/tmp/result"
-  creates "/tmp/result"
-  action :run
+template "/etc/hostname" do
+  source "hostname.erb"
+  notifies :run, resources(:service => "hostname")
 end
 
-# package "git-core" do
-  # action :install
-# end
-# package "lv" do
-  # action :install
-# end
-# add the Nginx PPA; grab key from keyserver
-# apt_repository "nginx-php" do
-  # uri "http://ppa.launchpad.net/nginx/php5/ubuntu"
-  # distribution node['lsb']['codename']
-  # components ["main"]
-  # keyserver "keyserver.ubuntu.com"
-  # key "C300EE8C"
-  # action :add
-# end
-
-# apt_repository "nginx-php" do
-  # uri "http://ppa.launchpad.net/nginx/php5/ubuntu"
-  # distribution "#{node[:lsb][:codename]}"
-  # components %w(main restricted universe multiverse)
-# end
-
-__END__
-# lucid
-deb http://172.27.98.16/cloud/latest/ubuntu lucid main restricted universe multiverse
-deb http://172.27.98.16/cloud/latest/ubuntu lucid-updates main restricted universe multiverse
-deb http://172.27.98.16/cloud/latest/ubuntu lucid-security main restricted universe multiverse
-
-# openstack
-deb http://172.27.98.16/cloud/latest/openstack/nova-core/release/ubuntu lucid main
-
-__END__
-require "rubygems"
-require "colored"
-def info(&blk) log("#{blk.call.to_s}".magenta){ level :info } end
-def fatal(&blk) log("#{blk.call.to_s}".red){ level :fatal } end
-# :info, :warn, :debug, :error, or :fatal
-info { "#{node.cpu[:total]}" }
-fatal { "#{node.cpu[:total]}" }
-fatal { "HOGEHOGE" }
-info { packages.inspect }
+template "/etc/hosts" do
+  source "hosts.erb"
+end
